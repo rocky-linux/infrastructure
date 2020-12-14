64ec70dee59cf3993f86e3a77cf8880888865fb7
Chris Cowley <chris.cowley@fr.clara.net>
2020-12-14 17:03:09 +0100

    added an architecture diagram for Prometheus

---
+++ architecture/monitoring/README.md
@@ -0,0 +1,6 @@
+Use [python-diagrams](https://diagrams.mingrammer.com) to build construct the diagram.
+
+```
+pip install --user diagrams
+python ./prometheus-mvp.py
+```
---
+++ architecture/monitoring/prometheus-mvp.py
@@ -0,0 +1,61 @@
+from diagrams import Diagram, Cluster, Edge
+from diagrams.aws.compute import EC2
+# from diagrams.aws.database import RDS
+# from diagrams.aws.network import ELB
+# from diagrams.aws.security import IAMRole
+from diagrams.aws.general import General
+from diagrams.aws.network import ELB
+from diagrams.onprem.monitoring import Grafana, Prometheus
+# from diagrams.onprem.network import Nginx
+from diagrams.onprem.compute import Server
+from diagrams.saas.alerting import Pushover
+from diagrams.saas.chat import Slack
+
+graph_attr = {
+        }
+
+node_attr = {
+        }
+
+with Diagram("Prometheus MVP",
+             show=False,
+             direction="TB",
+             outformat="png",
+             graph_attr=graph_attr,
+             node_attr=node_attr):
+
+    with Cluster("Rocky VPC"):
+        with Cluster("AWS services"):
+            aws_group = [
+                    EC2("service01"),
+                    EC2("service02"),
+                    ]
+        with Cluster("metrics host"):
+            metrics = Prometheus("metrics")
+            alertmanager = Prometheus("alertmanager")
+            dashboard = Grafana("monitoring")
+            metrics << dashboard
+            metrics >> alertmanager
+    metrics >> Edge(style="dashed",
+                    label="ec2 read permissions") >> General("AWS API")
+
+    alertmanager >> Edge(style="dashed",
+                         label="non-critical") >> Slack("rocky-alerts")
+    alertmanager >> Edge(style="dashed",
+                         label="critical") >> Pushover("tbd")
+    ELB("metrics.rockylinux.org") >> Edge(label="TCP3000") >> dashboard
+    with Cluster("Cloudvider"):
+        cloudvider_group = [
+                Server("server01"),
+                Server("server02"),
+                ]
+
+    with Cluster("Spry Servers"):
+        spry_group = [
+                Server("server01"),
+                Server("server02"),
+                ]
+
+    metrics >> aws_group
+    metrics >> spry_group
+    metrics >> cloudvider_group
